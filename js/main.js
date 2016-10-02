365f32f8781a5f220e0361622e7d659771ceea7d
Oeromilk <oero.mccomb@gmail.com>
2016-10-02 00:38:36 -0400

    About 50% or more done

--- js/main.js
+++ js/main.js
@@ -1,15 +1,50 @@
 (function(){
   "use strict";
 
+  window.onload = function() {
+
+  var turns = 10;
+  var guess;
+  var guesses = [];
+
+  var lives = document.getElementById('lives');
+  var textBox = document.getElementById('text-box');
+
   function randomWord(commonWords){
-  var randomIndex = commonWords[Math.floor(Math.random()*commonWords.length)];
-  return randomIndex;
+    var randomIndex = commonWords[Math.floor(Math.random()*commonWords.length)];
+    return randomIndex;
   }
 
   var word = commonWords.filter(function(words){
     return words.length>2;
   });
 
-  console.log(randomWord(word));
+  var guessDisplay = new Array(word.length);
 
+  for (var i = 0; i < guessDisplay.length; i++){
+    guessDisplay[i] = "_ ";
+  }
+
+  function printGuessDisplay(){
+    for (var i = 0; i < word.length; i++){
+      var cover = document.createTextNode(guessDisplay[i]);
+      textBox.appendChild(cover);
+    }
+  }
+
+  textBox.innerHTML = randomWord(word).split('');
+
+  function lifeBar(){
+    lives.innerHTML = "You have " + turns + " turns remaining.";
+    if (lives < 1){
+      lives.innerHTML = "Game Over";
+    }
+    for (var i = 0; i < guesses.length; i++){
+      if (pickedWord === guesses.length){
+        lives.innerHTML = "You Win!";
+      }
+    }
+  }
+
+  }
 }());
