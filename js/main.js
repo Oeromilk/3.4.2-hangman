f5946b84b309458ab4c14be8589c2db47bb2fb4c
Oeromilk <oero.mccomb@gmail.com>
2016-10-03 16:46:14 -0400

    Completed working project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 (function(){
   "use strict";
 
-  var turns = 0;
+  var turn = 8;
 
   var userInterface = document.getElementById('userInterface');
+  var guessThis = document.getElementById("guessThis");
+  var guess = document.getElementById('userInput');
+  var turns = document.getElementById('turnsBox');
+
+  turns.textContent = turn;
 
   function randomWord(commonWords){
       var randomIndex = commonWords[Math.floor(Math.random()*commonWords.length)];
@@ -14,7 +19,7 @@
       return words.length>2;
     });
 
-  var guessThisWord = randomWord(word);
+  var guessThisWord = randomWord(word).split('');
 
   var wordCover = new Array(guessThisWord.length);
 
@@ -22,50 +27,69 @@
     wordCover[i] = "_ ";
   }
 
-
   console.log(guessThisWord);
   console.log(wordCover);
 
   function printGuessWord(){
     for (var i = 0; i < wordCover.length; i++){
-      var guessThis = document.getElementById("guessThis");
       var holder = document.createTextNode(wordCover[i]);
       guessThis.appendChild(holder);
     }
   }
+  function clearInput(){
+    guess.value = "";
+  }
 
   var checkGuess = function(){
-    //var f = document.userInterface;
-    //var userInterface = elements["userInput"];
-    var guess = document.getElementById('userInput');
     for (var i = 0; i < guessThisWord.length; i++){
-      if (guessThisWord === guess){
-        wordCover[i] = guess + " ";
+      if (guessThisWord[i] === guess.value){
+        wordCover[i] = guess.value;
+        guessThis.textContent = '';
+        //clearInput();
+        printGuessWord();
+        console.log(guess.value);
+        console.log(wordCover);
         var truth = true;
       }
-      guess.value = "";
     }
+    if(guessThisWord.indexOf(guess.value) == -1){
+      console.log(turn);
+      turn -= 1;
+      turns.textContent = turn;
+
+    }
+    if (turn === 0){
+      window.alert("You have been hung!");
+    }
+
+    if(document.getElementById('guessThis').textContent == guessThisWord.join('')){
+      // finished = true;
+      alert('you win!');
+    }
+
+      // console.log(document.getElementById('guessThis').textContent);
+      // console.log(guessThisWord.join(''));
   }
 
   document.getElementById("userGuess").addEventListener("click", checkGuess);
+  guess.addEventListener("click", clearInput);
 
   var guessThis = document.getElementById("guessThis");
   guessThis.innerHTML = "";
   printGuessWord();
 
-  var finished = false;
-  for (var i = 0; i < wordCover; i++){
-    if(wordCover[i] === "_ "){
-      finished = true;
-    }
-  }
-  if(finished){
-    window.alert("You Win!");
-  }
+  // var finished;
+  // for (var i = 0; i < wordCover; i++){
+  //   if(document.getElementBy('guessThis').textContent === wordCover){
+  //     // finished = true;
+  //     alert('you win!')
+  //   }
+  // }
+  // if(finished){
+  //   window.alert("You Win!");
+  // }
 
-  if (turns === 6){
-    window.alert("You have been hung!");
-  }
+
 
 
 }());
